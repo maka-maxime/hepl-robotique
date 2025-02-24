77bfb5426d13580f89ff8f0f86e82c3d3c9123d8
Thb <thibault.bernard@student.hepl.be>
2025-02-24 14:54:36 +0100

    Prototype moteur init

---
+++ Prototype_Moteur/Prototype_Moteur.ino
@@ -0,0 +1,98 @@
+#define MOTOR_IN1 2
+#define MOTOR_IN2 3
+#define MOTOR_IN3 4
+#define MOTOR_IN4 5
+enum move{
+  forward,
+  backward,
+  left,
+  rigth,
+  stop  
+};
+void setup() {
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600); // Initialiser la communication série
+    Serial.println("DRV8871 test");
+  
+    pinMode(MOTOR_IN1, OUTPUT);
+    pinMode(MOTOR_IN2, OUTPUT);
+    pinMode(MOTOR_IN3, OUTPUT);
+    pinMode(MOTOR_IN4, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("haut");
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("bas");
+    delay(1000);
+
+
+    Moteurs(forward,1000);
+    Moteurs(stop,10000);
+  // // forward full speed for one second
+  // delay(1000);
+  
+
+  //         // ramp down forward
+  //     for (int i=255; i>=0; i--) {
+  //       analogWrite(MOTOR_IN2, i);
+  //       delay(10);
+  //     }
+
+
+  // // backward full speed for one second
+  // delay(1000);
+
+  // // ramp down backward
+  // for (int i=255; i>=0; i--) {
+  //   analogWrite(MOTOR_IN1, i);
+  //   delay(10);
+  // }
+}
+
+void Moteurs(move sense,int time)
+{
+  switch(sense)
+  {
+    case forward:
+      // ramp up forward
+      digitalWrite(MOTOR_IN1, LOW);
+      digitalWrite(MOTOR_IN4, LOW);
+      for (int i=0; i<255; i++) {
+        analogWrite(MOTOR_IN2, i);
+        analogWrite(MOTOR_IN3, i);
+        delay(10);
+      }
+      break;
+    case backward:
+        // ramp up backward
+      digitalWrite(MOTOR_IN2, LOW);
+      digitalWrite(MOTOR_IN3,LOW);
+      for (int i=0; i<255; i++) {
+        analogWrite(MOTOR_IN1, i);
+        analogWrite(MOTOR_IN4,i);
+        delay(10);
+      }
+      break;
+    case left:            // voir si on fait pas tourner les roues de gauche en arrière
+      digitalWrite(MOTOR_IN3, LOW);
+      for (int i=0; i<255; i++) {        
+        analogWrite(MOTOR_IN4, i);
+        delay(10);
+      }
+      break;
+    case rigth:
+      digitalWrite(MOTOR_IN1, LOW);
+      for (int i=0; i<255; i++) {
+        analogWrite(MOTOR_IN2, i);
+        delay(10);
+      }
+      break;
+    case stop:
+      break;    
+  }
+  delay(time);
+
+}
