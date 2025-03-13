3d79770bd44f7966ea9d02e45a43aeaaaea68eac
Thb <thibault.bernard@student.hepl.be>
2025-03-13 14:50:07 +0100

    ajout de un capteur distance

--- Prototype_Moteur/Prototype_Moteur.ino
+++ Prototype_Moteur/Prototype_Moteur.ino
@@ -2,89 +2,85 @@
 #define MOTOR_IN2 3
 #define MOTOR_IN3 4
 #define MOTOR_IN4 5
-enum move{
+#define ULTRASON_TRIG1 21
+#define ULTRASON_ECHO1 22
+
+enum move {
   forward,
   backward,
   left,
-  rigth,
-  stop  
+  right,
+  stop
 };
+
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
-    Serial.begin(9600); // Initialiser la communication série
-    Serial.println("DRV8871 test");
-  
-    pinMode(MOTOR_IN1, OUTPUT);
-    pinMode(MOTOR_IN2, OUTPUT);
-    pinMode(MOTOR_IN3, OUTPUT);
-    pinMode(MOTOR_IN4, OUTPUT);
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  pinMode(ULTRASON_TRIG1, OUTPUT);
+  digitalWrite(ULTRASON_TRIG1, LOW);
+  pinMode(ULTRASON_ECHO1, INPUT);
+
+  pinMode(MOTOR_IN1, OUTPUT);
+  pinMode(MOTOR_IN2, OUTPUT);
+  pinMode(MOTOR_IN3, OUTPUT);
+  pinMode(MOTOR_IN4, OUTPUT);
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);
-    Serial.println("haut");
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    Serial.println("bas");
-    delay(1000);
+  float distance = getDistance();
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
 
+  if (distance < 10) { // Si un obstacle est trop proche
+    Moteurs(stop, 0);
+    Serial.println("Stop !!!");
+  } else {
+    Moteurs(forward, 0);
+  }
 
-    //Moteurs(forward,1000);
-    //Moteurs(stop,10000);
-    Moteurs(left,1000);
-    Moteurs(stop,10000);
-      
-
-
+  delay(500);
 }
 
-void Moteurs(move sense,int time)
-{
-  switch(sense)
-  {
+float getDistance() {
+  digitalWrite(ULTRASON_TRIG1, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(ULTRASON_TRIG1, LOW);
+  int duration = pulseIn(ULTRASON_ECHO1, HIGH);
+  return duration / 58.0;
+}
+
+void Moteurs(move sense, int time) {
+  switch (sense) {
     case forward:
-      // ramp up forward
       digitalWrite(MOTOR_IN1, LOW);
       digitalWrite(MOTOR_IN4, LOW);
-      for (int i=0; i<255; i++) {
-        analogWrite(MOTOR_IN2, i);
-        analogWrite(MOTOR_IN3, i);
-        delay(10);
-      }
+      analogWrite(MOTOR_IN2, 255);
+      analogWrite(MOTOR_IN3, 255);
       break;
     case backward:
-        // ramp up backward
       digitalWrite(MOTOR_IN2, LOW);
-      digitalWrite(MOTOR_IN3,LOW);
-      for (int i=0; i<255; i++) {
-        analogWrite(MOTOR_IN1, i);
-        analogWrite(MOTOR_IN4,i);
-        delay(10);
-      }
+      digitalWrite(MOTOR_IN3, LOW);
+      analogWrite(MOTOR_IN1, 255);
+      analogWrite(MOTOR_IN4, 255);
       break;
-    case left:            // voir si on fait pas tourner les roues de gauche en arrière
-      digitalWrite(MOTOR_IN2,LOW);
+    case left:
+      digitalWrite(MOTOR_IN2, LOW);
       digitalWrite(MOTOR_IN4, LOW);
-      for (int i=0; i<255; i++) {        
-        analogWrite(MOTOR_IN3, i);
-        analogWrite(MOTOR_IN1,i);
-        delay(10);
-      }
+      analogWrite(MOTOR_IN3, 255);
+      analogWrite(MOTOR_IN1, 255);
       break;
-    case rigth:
+    case right:
       digitalWrite(MOTOR_IN1, LOW);
-      for (int i=0; i<255; i++) {
-        analogWrite(MOTOR_IN2, i);
-        delay(10);
-      }
+      analogWrite(MOTOR_IN2, 255);
       break;
     case stop:
       digitalWrite(MOTOR_IN1, LOW);
       digitalWrite(MOTOR_IN2, LOW);
       digitalWrite(MOTOR_IN3, LOW);
       digitalWrite(MOTOR_IN4, LOW);
-      break;    
+      break;
   }
-  delay(time);
-
+  if (time > 0) delay(time);
 }
