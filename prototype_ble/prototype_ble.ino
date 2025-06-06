67a38506594a0c20b75b5ef70a00156aac96d787
Maxime Maka <maxime.maka@student.hepl.be>
2025-06-06 12:23:39 +0200

    maj prototype BLE.

--- prototype_ble/prototype_ble.ino
+++ prototype_ble/prototype_ble.ino
@@ -1,5 +1,6 @@
 #define BLE_TX 16
 #define BLE_RX 17
+#define BLE_STATE 2
 #define LED_PIN 8
 
 uint8_t led_state;
@@ -10,14 +11,23 @@ size_t bytes_read;
 void setup() {
   Serial.begin(9600);
   Serial2.begin(9600);
+  pinMode(BLE_STATE, INPUT);
   pinMode(LED_PIN, OUTPUT);
   pinMode(BLE_TX, OUTPUT);
   pinMode(BLE_RX, INPUT);
   led_state = 0;
-  Serial.println("Waiting for a connection...");
 }
 
 void loop() {
+
+  if (LOW == digitalRead(BLE_STATE))
+  {
+    Serial.println("Waiting for a connection...");
+    while (LOW == digitalRead(BLE_STATE)) {
+    }
+    Serial.println("Device connected.");
+  }
+
   if (Serial2.available() != 0)
   {
     bytes_read = Serial2.readBytes(buffer, 31);
@@ -25,5 +35,6 @@ void loop() {
     Serial.println(bytes_read);
     Serial.print("Data received: ");
     Serial.println((const char *)buffer);
+    Serial2.println("ACK\n");
   }
 }
